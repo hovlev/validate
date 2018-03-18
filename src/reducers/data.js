6410b38c051ee1468554fa40537fbbb9a6380892
Mick Horler <m.p.horler@gmail.com>
2018-03-18 18:24:46 +0000

    Modified jsx and reducer, highlight whether the offer is below or above the expected value

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,13 +1,36 @@
-import { assoc } from 'ramda';
+import { __, add, assoc, assocPath, lt, gt, merge, prop } from 'ramda';
 
 const init = {
-  user: {}
+  offer: {
+    currency: '£',
+    min: 500,
+    max: 50000,
+    value: 0,
+    below: false,
+    above: false,
+  },
+  user: {},
+};
+
+const validateOffer = (value, { offer }) => {
+  const below = lt(value, prop('min', offer));
+  const above = gt(value, prop('max', offer));
+  return merge(offer, {
+    below,
+    above,
+    validated: !below && !above,
+    value,
+  });
 };
 
 export default (state = init, { payload, type }) => {
   switch (type) {
     case 'SUBMIT':
       return state;
+    case 'OFFER_CHANGE':
+      return merge(state, {
+        offer: validateOffer(payload, state),
+      });
     case 'USER_LOADED':
       return assoc('user', payload, state);
     default: {
--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import actions from '../../actions';
+import styles from './index.css';
 
-const App = ({ user }) => (
+const HelpOffer = ({ offer }) =>
+  <p>The <span className={offer.below ? styles.invalid : styles.valid}>{`minimum offer is ${offer.currency}${offer.min}`}</span> and the <span className={offer.above ? styles.invalid : styles.valid}>{`maximum offer is ${offer.currency}${offer.max}`}</span>.</p>;
+
+const App = ({ offer, user, offerChange }) => (
   <section>
     <h1>Validate</h1>
-    <p>{user.id} {user.name}</p>
+    <form>
+      <fieldset>
+        <label htmlFor="offer">{`Hi ${user.name}, what's your weekly offer?`}</label>
+        <p>{offer.currency}</p>
+        <input
+          onChange={e => offerChange(e.target.value)}
+          id="offer"
+          min={offer.min}
+          max={offer.max}
+          placeholder={offer.min}
+          name="offer"
+          type="number"
+          value={offer.value}
+        />
+        {offer.value > 0 &&
+          <HelpOffer offer={offer} />
+        }
+        {offer.validated &&
+          <p>Your offer of {offer.currency}{offer.value} looks good! Ready to submit?</p>
+        }
+      </fieldset>
+    </form>
   </section>
 );
 
 export default connect(state => ({
   user: state.data.user,
+  offer: state.data.offer,
+}), dispatch => ({
+  offerChange: value => dispatch({ type: actions.OFFER_CHANGE, payload: value }),
 }))(App);
