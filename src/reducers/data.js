f6ae32a987af9b501f81c4be1836d4103c188ac6
Mick Horler <m.p.horler@gmail.com>
2018-03-19 01:33:57 +0000

    Added populate functionality

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,4 +1,4 @@
-import { assoc, assocPath, contains, flatten, lt, gt, merge, pipe, prop, test, toPairs } from 'ramda';
+import { assoc, assocPath, contains, flatten, lt, gt, merge, path, pipe, prop, test, toPairs } from 'ramda';
 import validationRules from '../constants/validation';
 
 const init = {
@@ -6,7 +6,7 @@ const init = {
     currency: '£',
     min: 500,
     max: 50000,
-    value: 0,
+    value: '',
     below: false,
     above: false,
   },
@@ -19,6 +19,19 @@ const init = {
   canSubmit: false,
   hasSubmitted: false,
   user: {},
+  card: {
+    number: '',
+    expiryDate: '',
+    cvv: '',
+    postCode: '',
+  },
+  defaults: {
+    value: 1000,
+    number: '4111411141114111',
+    expiryDate: '12/22',
+    cvv: '0123',
+    postCode: 'SN11NS',
+  },
 };
 
 const validateOffer = (value, { offer }) => {
@@ -35,6 +48,36 @@ const validateOffer = (value, { offer }) => {
 const validateCreditCard = ({ value, rule }, { validation }) =>
   assoc(rule, test(prop(rule, validationRules), value), validation);
 
+const populate = state => {
+  const offer = prop('offer', state);
+  return merge(state, {
+    offer: merge(offer, {
+      value: path(['defaults', 'value'], state),
+      validated: true,
+    }),
+    card: prop('defaults', state),
+    validation: prop('defaults', state),
+    canSubmit: true,
+  });
+};
+
+const creditCardChange = (payload, state) => {
+  const isInvalid = pipe(
+    toPairs,
+    flatten,
+    contains(false)
+  );
+  const validatedCreditCardFields = validateCreditCard(payload, state);
+  const card = prop('card', state);
+  return merge(state, {
+    validation: validatedCreditCardFields,
+    canSubmit: !isInvalid(validatedCreditCardFields),
+    card: merge(card, {
+      [prop('rule', payload)]: prop('value', payload),
+    }),
+  });
+};
+
 export default (state = init, { payload, type }) => {
   switch (type) {
     case 'SUBMIT':
@@ -44,20 +87,13 @@ export default (state = init, { payload, type }) => {
         offer: validateOffer(payload, state),
       });
     case 'CREDIT_CARD_CHANGE':
-      const isInvalid = pipe(
-        toPairs,
-        flatten,
-        contains(false)
-      );
-      const validatedCreditCardFields = validateCreditCard(payload, state);
-      return merge(state, {
-        validation: validatedCreditCardFields,
-        canSubmit: !isInvalid(validatedCreditCardFields),
-      });
+      return creditCardChange(payload, state);
     case 'OFFER_SUBMIT':
       return assoc('hasSubmitted', true, state);
     case 'USER_LOADED':
       return assoc('user', payload, state);
+    case 'POPULATE':
+      return populate(state);
     default: {
       return state;
     }
